661d4ada0c47a6637e5e8f0927935b9ea64d6eae
yaswanth-godspeed <yaswanth@godspeed.systems>
2023-11-10 15:52:40 +0530

    added cors

--- src/eventsources/types/express.ts
+++ src/eventsources/types/express.ts
@@ -1,17 +1,2 @@
-import { PlainObject } from "@godspeedsystems/core";
-import { EventSource } from "@godspeedsystems/plugins-express-as-http";
-// You have to install cors package,  `npm install cors`
-import cors from 'cors';
-
-class MyEventSource extends EventSource {
-  async initClient(): Promise<PlainObject> {
-    const client = await super.initClient();
-
-    // adding a cors middleare
-    client.use(cors());
-
-    return client;
-  }
-};
-
-export default MyEventSource;
+import  EventSource  from "express_yash";
+export default EventSource;
